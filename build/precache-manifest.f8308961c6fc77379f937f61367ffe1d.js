41e90287c4c0d274c8bf08fda19bf56dbe1cc13e
Sandrine Babila Nubia <sandrine@Sandrines-MacBook-Pro.local>
2020-07-28 21:21:17 -0700

    initial commit

---
+++ build/precache-manifest.f8308961c6fc77379f937f61367ffe1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3fe70730a72f9111837f10d4ac5f604",
+    "url": "/User_Directory/index.html"
+  },
+  {
+    "revision": "3b7207dfb84d2b1ec144",
+    "url": "/User_Directory/static/css/main.9faf5e4f.chunk.css"
+  },
+  {
+    "revision": "4a4a4b59ccec60f8c8ee",
+    "url": "/User_Directory/static/js/2.7e2895e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User_Directory/static/js/2.7e2895e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7207dfb84d2b1ec144",
+    "url": "/User_Directory/static/js/main.ce993416.chunk.js"
+  },
+  {
+    "revision": "f08cc25b246f2cc839c2",
+    "url": "/User_Directory/static/js/runtime-main.18036e80.js"
+  }
+]);
